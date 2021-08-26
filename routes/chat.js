619edc04fb5e5354d6cfabc38de11dae898b8ab3
dntjd7701 <dntjd7701@gmail.com>
2021-08-26 12:36:11 +0900

    #Dr.[MERGE]: merge, 통합

--- routes/chat.js
+++ routes/chat.js
@@ -3,9 +3,16 @@ const controller = require('../controllers/chat');
 const auth = require('./authorized');
 
 const router = express.Router();
+// router.route('/roomlist/:userNo').get(auth("ROLE_USER"), controller.getRoomList);
+// router.route('/chatlist/:roomNo').get(auth("ROLE_USER"), controller.getChatList);
+// router.route('/message').post(auth("ROLE_USER"), controller.send);
+// router.route('/create').post(auth("ROLE_USER"),controller.create);
+// router.route('/setStatus').post(auth("ROLE_USER"), controller.updateStatus);
+
+
 router.route('/roomlist/:userNo').get(auth("ROLE_USER"), controller.getRoomList);
-router.route('/chatlist/:roomNo').get(auth("ROLE_USER"), controller.getChatList);
-router.route('/message').post(auth("ROLE_USER"), controller.send);
-router.route('/create').post(auth("ROLE_USER"),controller.create);
-router.route('/setStatus').post(auth("ROLE_USER"), controller.updateStatus);
+router.route('/chatlist/:roomNo').get(controller.getChatList);
+router.route('/message').post(controller.send);
+router.route('/create').post(controller.create);
+router.route('/setStatus').post(controller.updateStatus);
 module.exports = router;
