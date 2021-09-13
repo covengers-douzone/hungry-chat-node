0a7ca714a2bbe3a691490f0fa9678e55538e41dd
PDH <54939319+wow1186@users.noreply.github.com>
2021-09-13 09:45:05 +0900

    #Thor [FEAT] USER , UNKNOWN 기능 추가

--- routes/chat.js
+++ routes/chat.js
@@ -16,39 +16,39 @@ const upload = multer({storage:storage});
 
 //
 const router = express.Router();
-router.route('/roomlist/:userNo').get(controller.getRoomList);
-router.route('/chatlist/:roomNo/:offset/:limit').get(controller.getChatList);
-router.route('/chatlistCount/:roomNo/').get(controller.getChatListCount);
-router.route('/getChat/:chatNo').get(controller.getChat);
+router.route('/roomlist/:userNo').get(auth(["ROLE_USER"] , ["ROLE_UNKNOWN"]),controller.getRoomList);
+router.route('/chatlist/:roomNo/:offset/:limit').get(auth(["ROLE_USER"] , ["ROLE_UNKNOWN"]),controller.getChatList);
+router.route('/chatlistCount/:roomNo/').get(auth(["ROLE_USER"] , ["ROLE_UNKNOWN"]),controller.getChatListCount);
+router.route('/getChat/:chatNo').get(auth(["ROLE_USER"] , ["ROLE_UNKNOWN"]),controller.getChat);
 
-router.route('/chatlist/:roomNo').get( controller.getChatList);
-router.route('/message').post(upload.single( "file"), controller.send);
-router.route('/createRoom').post(controller.createRoom);
-router.route('/createParticipant').post(controller.createParticipant);
-router.route('/setStatus').post(controller.updateStatus);
-router.route('/getHeadCount').post(controller.getHeadCount);
-router.route('/updateSendNotReadCount').post(controller.updateSendNotReadCount);
-router.route('/updateRoomNotReadCount').post(controller.updateRoomNotReadCount);
-router.route('/updateLastReadAt').post(controller.updateLastReadAt);
-
-
+router.route('/chatlist/:roomNo').get(auth(["ROLE_USER"] , ["ROLE_UNKNOWN"]), controller.getChatList);
+router.route('/message').post(auth(["ROLE_USER"] , ["ROLE_UNKNOWN"]),upload.single( "file"), controller.send);
+router.route('/createRoom').post(auth(["ROLE_USER"] , ["ROLE_UNKNOWN"]),controller.createRoom);
+router.route('/createParticipant').post(auth(["ROLE_USER"]),controller.createParticipant);
+router.route('/setStatus').post(auth(["ROLE_USER"]),controller.updateStatus);
+router.route('/getHeadCount').post(auth(["ROLE_USER"]),controller.getHeadCount);
+router.route('/updateSendNotReadCount').post(auth(["ROLE_USER"]),controller.updateSendNotReadCount);
+router.route('/updateRoomNotReadCount').post(auth(["ROLE_USER"]),controller.updateRoomNotReadCount);
+router.route('/updateLastReadAt').post(auth(["ROLE_USER"]),controller.updateLastReadAt);
 
 
-router.route('/deleteChat').post( controller.deleteChat);
-router.route('/getFriendList').post(controller.getFriendList);
-router.route('/getFollowerList').post(controller.getFollowerList);
-router.route('/getOpenChatRoomList/:type').get(controller.getOpenChatRoomList);
 
-router.route('/addFriend').post( controller.addFriend);
-router.route('/deleteFriend').post( controller.deleteFriend);
-router.route('/getUserByNo/:userNo').get( controller.getUserByNo);
-router.route('/updateSettings').post(upload.single( "file"),  controller.updateSettings);
-router.route('/deleteUserInfo').post( controller.deleteUserInfo);
-router.route('/getLastReadNo').post(controller.getLastReadNo);
-router.route('/getLastReadNoCount').post(controller.getLastReadNoCount);
-router.route('/deleteChatNo/:chatNo').post(controller.deleteChatNo);
+
+router.route('/deleteChat').post( auth(["ROLE_USER"]),controller.deleteChat);
+router.route('/getFriendList').post(auth(["ROLE_USER"]),controller.getFriendList);
+router.route('/getFollowerList').post(auth(["ROLE_USER"]),controller.getFollowerList);
+router.route('/getOpenChatRoomList/:type').get(auth(["ROLE_USER"] , ["ROLE_UNKNOWN"]),controller.getOpenChatRoomList);
+
+router.route('/addFriend').post( auth(["ROLE_USER"]),controller.addFriend);
+router.route('/deleteFriend').post( auth(["ROLE_USER"]),controller.deleteFriend);
+router.route('/getUserByNo/:userNo').get( auth(["ROLE_USER"]),controller.getUserByNo);
+router.route('/updateSettings').post(auth(["ROLE_USER"]),upload.single( "file"),  controller.updateSettings);
+router.route('/deleteUserInfo').post( auth(["ROLE_USER"]),controller.deleteUserInfo);
+router.route('/getLastReadNo').post(auth(["ROLE_USER"] , ["ROLE_UNKNOWN"]),controller.getLastReadNo);
+router.route('/getLastReadNoCount').post(auth(["ROLE_USER"] , ["ROLE_UNKNOWN"]),controller.getLastReadNoCount);
+router.route('/deleteChatNo/:chatNo').post(auth(["ROLE_USER"]),controller.deleteChatNo);
 
 // layer 변경
-router.route('/joinRoom').post(controller.joinRoom);
-router.route('/leftRoom').post(controller.leftRoom);
+router.route('/joinRoom').post(auth(["ROLE_USER"] , ["ROLE_UNKNOWN"]),controller.joinRoom);
+router.route('/leftRoom').post(auth(["ROLE_USER"] , ["ROLE_UNKNOWN"]),controller.leftRoom);
 module.exports = router;
